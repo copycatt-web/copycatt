74f4982367624d0f32fb5137c266990077a2d1e8
itscopycatt <81062027+itscopycatt@users.noreply.github.com>
2025-12-18 00:09:40 -0800

    iunno

--- public/script.js
+++ public/script.js
@@ -2,6 +2,33 @@ const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
 const logo = document.getElementById('h1')
 
+async function getDiscography() {
+	try {
+		const response = await fetch('./bandcamp-discog.json');
+		if (!response.ok) throw new Error("He is not okay.");
+
+		const data = await response.json();
+
+		console.log("Successfully fetched bandcamp discog: ", data);
+		return data;
+
+	} catch (error) {
+		console.error("Error fetching bandcamp discog file: ", error)
+	}
+}
+
+getDiscography().then(data => {
+	const discog = data;
+	const container = document.getElementById('bandcamp-grid')
+	for(const track of discog){
+		console.log(track);
+		const frameToAdd = `<iframe class="bc-embed" style="border: 0; width: 200px; height: 200px;" src="https://bandcamp.com/EmbeddedPlayer/track=${track.id}/size=large/bgcol=222222/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="${track.url}">${track:name} by COPYCATT</a></iframe>`
+		container.insertAdjacentHTML('afterend', frameToAdd);
+	}
+});
+
+alert(discog)
+
 function navHome() {
 	location.reload()
 }
